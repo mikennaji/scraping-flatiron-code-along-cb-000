358f4992fe6073e83626623e323e82afc735ec07
Michael Nnaji <mnnaji3@uwo.ca>
2020-07-23 18:28:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,9 +18,10 @@ def make_courses
 
   self.get_courses.each do |post|
     course = Course.new
-    course.title = post.css('h2')
-    course.schedule = post.css('date')
-    course.description = post.css('p')
+    course.title = post.css('h2').text
+    course.schedule = post.css('date').text
+    course.description = post.css('p').text
+    
 
 
 
